9adbefb9649678ac5ee7c67e31fe1835087befe0
egin10 <wweusz@gmail.com>
2017-10-26 12:40:14 +0700

    add new list

--- todolist/app/views.py
+++ todolist/app/views.py
@@ -1,13 +1,38 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from .models import Todo
 
 # Create your views here.
 def index(request):
+    # Get data from Model
+    todos = Todo.objects.all()[:10]
+
     # return HttpResponse('Hello World') ==> static response
     data = {
-        'name' : 'Egin10'
+        'todos' : todos
     }
     return render(request, 'index.html', data)
+
+def details(request, id):
+    todo = Todo.objects.get(id=id)
+    data = {
+        'todo' : todo
+    }
+    return render(request, 'details.html', data)
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        #saving data
+        todo = Todo(title=title, text=text)
+        todo.save()
+
+        return redirect('/app')
+    else:
+        return render(request, 'add.html')
